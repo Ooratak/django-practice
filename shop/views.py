12f5ffd21917993f31cb7b6687c366bb26ed689b
Ooratak <alib3hjat1ardakan1@gmail.com>
2025-07-31 08:55:16 +0330

    chore: add some basics

---
+++ shop/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+# Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'shop/index.html', {'products': products})
