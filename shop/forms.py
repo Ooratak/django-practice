28a6e14f87bdf89e8b3468de459953e61b1af5cc
Ooratak <alib3hjat1ardakan1@gmail.com>
2025-08-01 21:00:25 +0330

    chore: create send_email function in shop/forms.py

---
+++ shop/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class ContactFrom(forms.Form):
+    name = forms.CharField(max_length=100, required=True, label='Your Name')
+    email = forms.EmailField(required=True, label='Your Email')
+    message = forms.CharField(widget=forms.Textarea, required=True, label='Your Message')
+
+    def send_email(self):
+        print(f"Sending email from {self.cleaned_data['email']} with message: {self.cleaned_data['message']}")
